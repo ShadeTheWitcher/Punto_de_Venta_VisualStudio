a478bc39dce389cb12ca8d898dfd1c3cbc259889
Matias Lovato <matii_seba_11@hotmail.com>
2023-09-10 16:01:49 -0300

    arreglos visuales de carga de memoria

--- CyberGear16/formLogin.cs
+++ CyberGear16/formLogin.cs
@@ -28,52 +28,56 @@ namespace CyberGear16
         }
 
 
-
-
-
-        private void button1_Click_1(object sender, EventArgs e)
+        private async Task IniciarSesionAsync(string usuario, string contraseña)
         {
+            BdCybergearContext context = null;
+
             try
             {
-                string usuario = textBox1.Text;
-                string contraseña = textBox2.Text;
+                context = new BdCybergearContext();
+                var usuarioEncontrado = await context.Usuarios
+                    .FirstOrDefaultAsync(u => u.Usuario1 == usuario && u.Pass == contraseña);
 
-                Task.Run(async () => //hemos envuelto la lógica de inicio de sesión en un Task.Run para ejecutarla en segundo plano y permitir el uso de await
+                if (usuarioEncontrado != null)
                 {
-                    using (var context = new BdCybergearContext()) // se instancia el contexto y lo englobamos en un usign para q se cierre auto
-                    {
-                        var usuarioEncontrado = await context.Usuarios
-                            .FirstOrDefaultAsync(u => u.Usuario1 == usuario && u.Pass == contraseña);
+                    int perfilId = usuarioEncontrado.PerfilId;
+                    string nombreUser = usuarioEncontrado.Nombre;
 
-                        if (usuarioEncontrado != null)
-                        {
-                            int perfilId = usuarioEncontrado.PerfilId;
-                            string nombreUser = usuarioEncontrado.Nombre;
+                    // Crea una instancia del formulario GUI_Principal
+                    GUI_Principal form2 = new GUI_Principal(perfilId, nombreUser, context);
 
-                            // Crea una instancia del formulario GUI_Principal
-                            GUI_Principal form2 = new GUI_Principal(perfilId, nombreUser, context);
+                    // Las credenciales son válidas, el usuario ha iniciado sesión
+                    MessageBox.Show("Inicio de sesión exitoso, bienvenido " + nombreUser + " tipoUsuario: " + perfilId);
 
-                            // Las credenciales son válidas, el usuario ha iniciado sesión
-                            MessageBox.Show("Inicio de sesión exitoso, bienvenido " + nombreUser + " tipoUsuario: " + perfilId);
+                    // Oculta el formulario actual (formLogin)
+                    this.Hide();
 
-                            // Oculta el formulario actual (formLogin)
-                            this.Hide();
-
-                            // Muestra el formulario GUI_Principal
-                            form2.Show();
-                        }
-                        else
-                        {
-                            // Las credenciales son incorrectas
-                            MessageBox.Show("Nombre de usuario o contraseña incorrectos");
-                        }
-                    }
-                });
+                    // Muestra el formulario GUI_Principal
+                    form2.Show();
+                }
+                else
+                {
+                    // Las credenciales son incorrectas
+                    MessageBox.Show("Nombre de usuario o contraseña incorrectos");
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message);
             }
+            finally
+            {
+                context?.Dispose(); // Asegúrate de liberar los recursos del contexto
+            }
+        }
+
+
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            string usuario = textBox1.Text;
+            string contraseña = textBox2.Text;
+            await IniciarSesionAsync(usuario, contraseña);
         }
     }
 }
