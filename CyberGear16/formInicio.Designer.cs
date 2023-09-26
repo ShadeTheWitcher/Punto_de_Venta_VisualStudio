c427d3a23c581e4292313abd3aaf226533243c55
Matias Lovato <matii_seba_11@hotmail.com>
2023-09-26 20:11:09 -0300

    Pantalla_Bienvenida update

--- CyberGear16/formInicio.Designer.cs
+++ CyberGear16/formInicio.Designer.cs
@@ -28,54 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formInicio));
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
-            // label1
+            // pictureBox2
             // 
-            label1.Anchor = AnchorStyles.None;
-            label1.AutoSize = true;
-            label1.Font = new Font("Lucida Calligraphy", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(503, 105);
-            label1.Margin = new Padding(4, 0, 4, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(192, 37);
-            label1.TabIndex = 1;
-            label1.Text = "Bienvenido";
-            label1.TextAlign = ContentAlignment.TopCenter;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Anchor = AnchorStyles.None;
-            pictureBox1.Image = Properties.Resources._7be5f80bdd244197bed72c7c0f28e283;
-            pictureBox1.Location = new Point(491, 196);
-            pictureBox1.Margin = new Padding(4, 3, 4, 3);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(216, 232);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            pictureBox2.Anchor = AnchorStyles.None;
+            pictureBox2.ErrorImage = null;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(-1, 179);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(1187, 387);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // formInicio
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gainsboro;
             ClientSize = new Size(1183, 737);
-            Controls.Add(label1);
-            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            ForeColor = SystemColors.ControlText;
             FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(4, 3, 4, 3);
             Name = "formInicio";
             Text = "Inicio";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private PictureBox pictureBox1;
-        private Label label1;
+        private PictureBox pictureBox2;
     }
 }
